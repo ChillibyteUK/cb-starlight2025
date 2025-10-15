4691dbaabcddffb26cc19f63de1f81e40e81dcf0
ChillibyteUK <dan@chillibyte.co.uk>
2025-10-15 12:02:58 +0100

    doclist

--- blocks/cb-doclist.php
+++ blocks/cb-doclist.php
@@ -13,41 +13,43 @@ defined( 'ABSPATH' ) || exit;
 		<?php
 		if ( have_rows( 'documents' ) ) {
 			?>
-			<div class="row g-4">
+			<div class="row g-5">
 				<?php
 				while ( have_rows( 'documents' ) ) {
 					the_row();
 					$doc  = get_sub_field( 'file' );
-					var_dump($doc);
 					$icon = 'application/pdf' === $doc['mime_type'] ? 'fa-file-pdf' : 'fa-file';
 					?>
-					<div class="col-md-4">
-						<a class="doclist__item d-block p-3 h-100 has-left-border" href="<?= esc_url( $doc['url'] ); ?>" target="_blank" rel="noopener">
+					<div class="col-md-3">
+						<a class="doclist__item d-flex flex-column h-100 has-black-color" href="<?= esc_url( $doc['url'] ); ?>" target="_blank" rel="noopener">
 							<?php
-							// display pdf thumbnail if pdf
-							if ( 'application/pdf' === $doc['mime_type'] ) {
-								$pdf_path = get_attached_file( $doc['id'] );
-								$upload_dir = wp_upload_dir();
-								$pdf_relative_path = str_replace( $upload_dir['basedir'], '', $pdf_path );
-								$thumbnail_url = $upload_dir['baseurl'] . '/thumbnails' . $pdf_relative_path . '.png';
-								if ( file_exists( $thumbnail_path ) ) {
-									echo '<img src="' . esc_url( $thumbnail_url ) . '" alt="' . esc_attr( $doc['title'] ) . '" class="img-fluid mb-2">';
-								}
-							}
-							?>
-							<h3 class="h5 mb-2"><i class="fas <?= esc_attr( $icon ); ?>"></i> <?= esc_html( $doc['title'] ); ?></h3>
-							<?php
-							if ( get_sub_field( 'file_name' ) ) {
+							$attachment_id = $doc['id'];
+							$thumb         = wp_get_attachment_image_src( $attachment_id, 'large' );
+							if ( $thumb ) {
 								?>
-								<p class="mb-0"><?= esc_html( get_sub_field( 'file_name' ) ); ?></p>
-								<?php
-							}
-							if ( get_sub_field( 'file_description' ) ) {
-								?>
-								<p class="mb-0"><?= esc_html( get_sub_field( 'file_description' ) ); ?></p>
+								<div class="doclist__image-wrapper">
+									<img src="<?= esc_url( $thumb[0] ); ?>" class="doclist__image" alt="">
+								</div>
 								<?php
 							}
 							?>
+							<div class="doclist__body d-flex flex-column flex-grow-1">
+								<h3 class="h3 mb-4 card-title">
+									<?= esc_html( get_sub_field( 'file_name' ) ? get_sub_field( 'file_name' ) : $doc['title'] ); ?>
+								</h3>
+								<div class="has-left-border">
+									<?php
+									if ( get_sub_field( 'file_description', get_the_ID() ) ) {
+										?>
+									<p class="card-text">
+										<?= wp_kses_post( get_sub_field( 'file_description', get_the_ID() ) ); ?>
+									</p>
+										<?php
+									}
+									?>
+									<span class="mt-auto align-self-start btn">Download</span>
+								</div>
+							</div>
 						</a>
 					</div>
 					<?php
