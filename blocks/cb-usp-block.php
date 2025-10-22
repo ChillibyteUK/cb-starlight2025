a464c70d08e95054faadacc28a41b9a99cfac03b
ChillibyteUK <dan@chillibyte.co.uk>
2025-10-22 12:38:14 +0100

    icons

--- blocks/cb-usp-block.php
+++ blocks/cb-usp-block.php
@@ -15,35 +15,35 @@ defined( 'ABSPATH' ) || exit;
 				<h2 class="h2 text-center mb-5">What makes us special</h2>
 				<div class="row g-5">
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-unrivalled-experience.svg' ); ?>" alt="Unrivalled Experience"></div>
 						<span class="usp-title">Unrivalled Experience</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-customised-project-management.svg' ); ?>" alt="Customised Project Management"></div>
 						<span class="usp-title">Customised Project Management</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-24-7-support.svg' ); ?>" alt="24/7 Support"></div>
 						<span class="usp-title">24/7 Support</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-secure-storage-facility.svg' ); ?>" alt="Secure Storage Facility"></div>
 						<span class="usp-title">Secure Storage Facility</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-free-design-visualisation.svg' ); ?>" alt="Free Design &amp; Visualisation"></div>
 						<span class="usp-title">Free Design &amp; Visualisation</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-high-quality-systems.svg' ); ?>" alt="High Quality Systems"></div>
 						<span class="usp-title">High Quality Systems</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-global-reach-local-reliability.svg' ); ?>" alt="Global Reach, Local Reliability"></div>
 						<span class="usp-title">Global Reach, Local Reliability</span>
 					</div>
 					<div class="col-sm-6 col-md-3 text-center">
-						<div class="usp-icon"><i class="fas fa-star fa-2x"></i></div>
+						<div class="usp-icon"><img src="<?= esc_url( get_stylesheet_directory_uri() . '/img/icons/icon-cost-optimisation.svg' ); ?>" alt="Cost Optimisation"></div>
 						<span class="usp-title">Cost Optimisation</span>
 					</div>
 				</div>
